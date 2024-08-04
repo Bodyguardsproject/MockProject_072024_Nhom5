ce190eabef7800f63c84490ac2a79aea970a22af
lequanglocdev <quanglocdev@gmail.com>
2024-08-04 17:50:45 +0700

    page admin

---
+++ MockProject-ReactJS/src/pages/private/AdminServices.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Login} from "../public/index"
+export const AdminServices = () => {
+  return (
+    <div className='h-full bg-slate-800 overflow-auto'>
+        <Login/>
+        <Login/>
+        <Login/>
+        <Login/>
+    </div>
+  )
+}
+
+export default AdminServices
---
+++ MockProject-ReactJS/src/pages/private/AdminTraining.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+ export const AdminTraining = () => {
+  return (
+    <div>AdminTraining</div>
+  )
+}
+
+export default AdminTraining
