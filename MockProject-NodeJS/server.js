6f08acdafc4dabb55f5ac191ac8a7a0dcb5d94ee
vudinhan2525 <vudinhan000@gmail.com>
2024-08-14 11:45:44 +0700

    upd

--- MockProject-NodeJS/server.js
+++ MockProject-NodeJS/server.js
@@ -1,1 +1,9 @@
-console.log(2);
+import app from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config.env" });
+
+const port = process.env.PORT || 9000;
+app.listen(port, () => {
+  console.log(`App is running in port ${port}`);
+});
