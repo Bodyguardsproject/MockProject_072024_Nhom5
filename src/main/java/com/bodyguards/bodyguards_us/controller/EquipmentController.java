2e7e7d40427a9e1e4bfef096518346806537dd36
NgocDanh <duongngocdanh6@gmail.com>
2024-08-15 11:11:51 +0700

    Create CRUD API Equipment v2

---
+++ src/main/java/com/bodyguards/bodyguards_us/controller/EquipmentController.java
@@ -0,0 +1,44 @@
+package com.bodyguards.bodyguards_us.controller;
+
+import com.bodyguards.bodyguards_us.dto.ApiResponse;
+import com.bodyguards.bodyguards_us.dto.EquipmentRequest;
+import com.bodyguards.bodyguards_us.entity.Equipment;
+import com.bodyguards.bodyguards_us.service.EquipmentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/equipment")
+public class EquipmentController {
+
+    @Autowired
+    EquipmentService equipmentService;
+
+    @GetMapping("/")
+    public ResponseEntity<ApiResponse<?>> getAllEquipment(){
+        return ResponseEntity.ok(ApiResponse.success(equipmentService.getAllEquipment()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<?>> getEquipmentById(@PathVariable Long id){
+        return ResponseEntity.ok(ApiResponse.success(equipmentService.getAllEquipment()));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponse<?>> createEquipment(@RequestBody @Valid EquipmentRequest request){
+        return ResponseEntity.ok(ApiResponse.success(equipmentService.createNewEquipment(request)));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ApiResponse<?>> updateEquipmentById(@PathVariable Long id, @RequestBody @Valid EquipmentRequest request){
+        return ResponseEntity.ok(ApiResponse.success(equipmentService.updateEquipmentById(id, request)));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ApiResponse<?>> deleteEquipmentById(@PathVariable Long id){
+        equipmentService.deleteEquipmentById(id);
+        return ResponseEntity.ok(ApiResponse.success("Delete Equipment Successfully"));
+    }
+}
---
+++ src/main/java/com/bodyguards/bodyguards_us/mapper/EquipmentMapper.java
@@ -0,0 +1,15 @@
+package com.bodyguards.bodyguards_us.mapper;
+
+import com.bodyguards.bodyguards_us.dto.EquipmentRequest;
+import com.bodyguards.bodyguards_us.entity.Equipment;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface EquipmentMapper {
+
+    public Equipment toEquipment(EquipmentRequest request);
+
+    void updateEquipment(EquipmentRequest request, @MappingTarget Equipment equipment);
+}
