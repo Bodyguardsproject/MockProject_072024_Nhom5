0e66b22595a7a63bce9b2e19c44ebcbd91b6cfed
lequanglocdev <quanglocdev@gmail.com>
2024-08-16 08:42:44 +0700

    fix code

--- MockProject-ReactJS/src/components/ComponentAdmin/SidebarAdmin.jsx
+++ MockProject-ReactJS/src/components/ComponentAdmin/SidebarAdmin.jsx
@@ -141,18 +141,7 @@ export const SidebarAdmin = () => {
               <span>Equipment</span>
             </Sidebar.Item>
           </Link>
-          <Link to="admin2">
-            {/* Sử dụng tham số tab trong URL */}
-            <Sidebar.Item active={tab === "admin" || !tab} icon={FaToolbox}>
-              <span>ABlskdksdbasdabdk</span>
-            </Sidebar.Item>
-          </Link>
-          <Link to="admin2">
-            {/* Sử dụng tham số tab trong URL */}
-            <Sidebar.Item active={tab === "admin" || !tab} icon={FaToolbox}>
-              <span>ABlskdksdbasdabdk</span>
-            </Sidebar.Item>
-          </Link>
+
           <SidebarProfile />
         </Sidebar.ItemGroup>
       </Sidebar.Items>
