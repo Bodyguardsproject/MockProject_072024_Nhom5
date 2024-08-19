7ffb4e63fb851a19fcebfd15e6521c64c491004f
WuocThun <binngoc1963@gmail.com>
2024-08-19 18:01:37 +0700

    update vite

--- Mockup_Laravel/resources/views/layouts/app.blade.php
+++ Mockup_Laravel/resources/views/layouts/app.blade.php
@@ -11,8 +11,8 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="{{ asset('js/app.js') }}" defer></script>    </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
